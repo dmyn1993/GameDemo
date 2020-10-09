a0d3ef0478e86b2330fb1d270556e6f3009d2b1f
dmyn1993 <dmyn199@gmail.com>
2020-10-09 18:17:04 +0800

    优化长按移动代码，手感更佳

--- app/src/main/java/com/dmyn/gamedemo/about.java
+++ app/src/main/java/com/dmyn/gamedemo/about.java
@@ -2,32 +2,33 @@ package com.dmyn.gamedemo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import java.util.Calendar;
+
 public class about extends Activity {
     RelativeLayout about;
     CustomMap customMap;
-    Button up,down,left,right;
-    public boolean isUpOnLongClick=false;
-    public boolean isDownOnLongClick=false;
-    public boolean isLeftOnLongClick=false;
-    public boolean isRightOnLongClick=false;
+    Button up, down, left, right;
+    public long clickTime = 0;
+    public Handler handler = new Handler();
+    public LongPressDirection longPressDirection = new LongPressDirection();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
         createCustomMap();
-        up=findViewById(R.id.up);
-        down=findViewById(R.id.down);
-        left=findViewById(R.id.left);
-        right=findViewById(R.id.right);
-        MyOnTouchListener mtl=new MyOnTouchListener();
+        up = findViewById(R.id.up);
+        down = findViewById(R.id.down);
+        left = findViewById(R.id.left);
+        right = findViewById(R.id.right);
+        MyOnTouchListener mtl = new MyOnTouchListener();
         up.setOnTouchListener(mtl);
         down.setOnTouchListener(mtl);
         left.setOnTouchListener(mtl);
@@ -37,101 +38,113 @@ public class about extends Activity {
 
     public void createCustomMap() {
         about = findViewById(R.id.about);
+        //实例化自定义视图
         customMap = new CustomMap(this);
+        //设置由控件内容决定当前控件的大小
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        //设置控件位于父控件的顶端
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        //设置垂直居中
         layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        //把规则应用于自定义视图
         customMap.setLayoutParams(layoutParams);
+        //把自定义视图添加到布局中
         about.addView(customMap);
     }
 
-    public class MyOnTouchListener implements View.OnTouchListener{
+    //设置控件触摸监听
+    public class MyOnTouchListener implements View.OnTouchListener {
 
         @Override
         public boolean onTouch(View v, MotionEvent event) {
-            if(event.getAction()==MotionEvent.ACTION_DOWN){
-                switch (v.getId()){
+            //如果触摸处于按下状态
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                //设置当前点击时间
+                clickTime = Calendar.getInstance().getTimeInMillis();
+                //判断按下了哪个按钮，0=上，1=下，2=左，3=右
+                switch (v.getId()) {
                     case R.id.up:
-                        LongUp longUp=new LongUp();
-                        isUpOnLongClick=true;
-                        longUp.start();
+                        //设置移动方向
+                        longPressDirection.setDirection(0);
                         break;
                     case R.id.down:
-                        LongDown longDown=new LongDown();
-                        isDownOnLongClick=true;
-                        longDown.start();
+                        longPressDirection.setDirection(1);
                         break;
                     case R.id.left:
-                        LongLeft longLeft=new LongLeft();
-                        isLeftOnLongClick=true;
-                        longLeft.start();
+                        longPressDirection.setDirection(2);
                         break;
                     case R.id.right:
-                        LongRight longRight =new LongRight();
-                        isRightOnLongClick=true;
-                        longRight.start();
+                        longPressDirection.setDirection(3);
                         break;
                 }
-            }else if (event.getAction()==MotionEvent.ACTION_UP || event.getAction()==MotionEvent.ACTION_OUTSIDE){
-                isUpOnLongClick=false;
-                isDownOnLongClick=false;
-                isLeftOnLongClick=false;
-                isRightOnLongClick=false;
+                //500毫秒后开始执行线程
+                handler.postDelayed(longPressDirection, 500);
+                //如果触摸处于抬起或移走状态
+            } else if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+                //取消线程
+                handler.removeCallbacks(longPressDirection);
+                //方向清零
+                longPressDirection.setDirection(4);
+                //如果触摸抬起时间小于等于500毫秒
+                if (Calendar.getInstance().getTimeInMillis() - clickTime <= 500) {
+                    //只移动一格
+                    switch (v.getId()) {
+                        case R.id.up:
+                            customMap.player.up();
+                            break;
+                        case R.id.down:
+                            customMap.player.down();
+                            break;
+                        case R.id.left:
+                            customMap.player.left();
+                            break;
+                        case R.id.right:
+                            customMap.player.right();
+                            break;
+                    }
+                    customMap.invalidate();
+                }
             }
             return false;
         }
     }
 
-    public class LongUp extends Thread{
-        public void run(){
-            while(isUpOnLongClick){
-                try {
+
+    //长按方向键线程
+    public class LongPressDirection implements Runnable {
+
+        //默认方向设为0-3以外的数字
+        private int direction = 4;
+
+        public int getDirection() {
+            return direction;
+        }
+
+        public void setDirection(int direction) {
+            this.direction = direction;
+        }
+
+        @Override
+        public void run() {
+
+            //根据direction来判断该向哪个方向移动
+            switch (this.getDirection()) {
+                case 0:
                     customMap.player.up();
-                    customMap.invalidate();
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-    public class LongDown extends Thread{
-        public void run(){
-            while(isDownOnLongClick){
-                try {
+                    break;
+                case 1:
                     customMap.player.down();
-                    customMap.invalidate();
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-    public class LongLeft extends Thread{
-        public void run(){
-            while(isLeftOnLongClick){
-                try {
+                    break;
+                case 2:
                     customMap.player.left();
-                    customMap.invalidate();
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-    public class LongRight extends Thread{
-        public void run(){
-            while(isRightOnLongClick){
-                try {
+                    break;
+                case 3:
                     customMap.player.right();
-                    customMap.invalidate();
-                    Thread.sleep(200);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                    break;
             }
+            customMap.invalidate();
+            //200毫秒后再次执行该线程
+            handler.postDelayed(longPressDirection, 200);
         }
     }
 
